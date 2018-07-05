55ab3430c793cf1ab5620a7e2fcb9cc7bbed7c2d
mrajani <mahesh@ionoinc.com>
2018-07-05 11:37:37 -0700

    knife configure removed, broken copy the ready made knife.rb to .chef directory

--- chefws.sh
+++ chefws.sh
@@ -4,9 +4,7 @@
 #
 
 chefserver="chefserver.iono.box"
-chefdkpkg=/vagrant/chefdk-0.18.30-1.el7.x86_64.rpm
 user=chefadmin
-userpw=vagrant
 username='Chef Admin'
 email=chefadmin@iono.box
 userpem=/vagrant/${user}.pem
@@ -15,16 +13,9 @@ orgpem=/vagrant/${org}-validator.pem
 
 dotchefdir=/home/vagrant/.chef
 mkdir $dotchefdir; touch $dotchefdir/knife.rb
+cp /vagrant/knife.rb ${dotchefdir}/knife.rb
 sudo chown -R vagrant:vagrant $dotchefdir
 
-knife configure \
-  --validation-client-name ${org}-validator \
-  --validation-key ${orgpem} \
-  --server-url https://${chefserver}/organizations/${org} \
-  --repository /home/vagrant/chef-repo \
-  --user ${user} \
-  --key  ${userpem} \
-  --config .chef/knife.rb -y
 knife ssl fetch
 knife ssl check
 
